48148797af49ba9e7909e4f9aacafd40bece163a
Inbaraj <ainbara@SYNTELINC.COM>
2021-08-11 11:51:35 +0530

    Added Mentor link in Navigation Bar

--- src/pages/Mentor/Mentor.js
+++ src/pages/Mentor/Mentor.js
@@ -3,6 +3,6 @@ import { Container } from 'react-bootstrap'
 
 export const Mentor = () => {
     return (
-        <Container><h1>Mentor Page Under Constuction...</h1></Container>
+        <Container><h1>Mentor Page Under Construction...</h1></Container>
     )
   }
